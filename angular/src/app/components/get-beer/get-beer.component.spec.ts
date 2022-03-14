52d293e7504d68f27aa78a28da87c1e77b553157
Timo Laak <1674055+tlaak@users.noreply.github.com>
2022-03-14 22:10:09 +1100

    Fix test setup

--- angular/src/app/components/get-beer/get-beer.component.spec.ts
+++ angular/src/app/components/get-beer/get-beer.component.spec.ts
@@ -1,26 +1,28 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { GetBeerComponent } from './get-beer.component';
 import { StoreModule, combineReducers } from '@ngrx/store';
-import { beerReducer } from 'app/reducers/beer.reducer';
+import { beerReducer } from '../../../app/reducers/beer.reducer';
 
 describe('GetBeerComponent', () => {
   let component: GetBeerComponent;
   let fixture: ComponentFixture<GetBeerComponent>;
 
-  beforeEach(waitForAsync(() => {
-    /**
-     * This component is subscribing to the @ngrx store. You need to use the
-     * StoreModule and define the name for the store in TestBed configuration
-     */
-    TestBed.configureTestingModule({
-      imports: [
-        StoreModule.forRoot({
-          beers: combineReducers(beerReducer),
-        }),
-      ],
-      declarations: [GetBeerComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      /**
+       * This component is subscribing to the @ngrx store. You need to use the
+       * StoreModule and define the name for the store in TestBed configuration
+       */
+      TestBed.configureTestingModule({
+        imports: [
+          StoreModule.forRoot({
+            beers: combineReducers(beerReducer),
+          }),
+        ],
+        declarations: [GetBeerComponent],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(GetBeerComponent);
@@ -35,13 +37,13 @@ describe('GetBeerComponent', () => {
   it('should have a subscription to beer reducer', () => {
     const beerSub = component['beerSub'];
     expect(beerSub).toBeDefined();
-    expect(beerSub.closed).toBeFalsy();
+    expect(beerSub?.closed).toBeFalsy();
   });
 
   it('should have no subscriptions when component has been destroyed', () => {
     component.ngOnDestroy();
 
     const beerSub = component['beerSub'];
-    expect(beerSub.closed).toBeTruthy();
+    expect(beerSub?.closed).toBeTruthy();
   });
 });
